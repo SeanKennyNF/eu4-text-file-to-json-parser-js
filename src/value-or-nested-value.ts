d8bfb33878e37d73ee7585ebbac22a808034e957
seank <seankenny6688@gmail.com>
2024-08-30 18:39:42 -0600

    revert experimental changes

--- src/value-or-nested-value.ts
+++ src/value-or-nested-value.ts
@@ -1,20 +1,11 @@
-export type ValueOrNestedValue<T> = string | string[] | { [key: string]: ValueOrNestedValue<T> } | ValueOrNestedValue<T>[];
+export type ValueOrNestedValue<T> = string | string[] | { [key: string]: ValueOrNestedValue<T> };
 
 export const valueOrNestedValueIsString = <T>(
   input: ValueOrNestedValue<T>
 ): input is string => typeof input === 'string';
 export const valueOrNestedValueIsStringArray = <T>(
   input: ValueOrNestedValue<T>
-): input is string[] => Array.isArray(input) && input.every((element) => typeof element === 'string');
-export const valueOrNestedValueIsNestedValueArray = <T>(
-  input: ValueOrNestedValue<T>
-): input is ValueOrNestedValue<T>[] => Array.isArray(input) && input.every((element) => typeof element !== 'string');
+): input is string[] => Array.isArray(input);
 export const valueOrNestedValueIsNestedValue = <T>(
   input: ValueOrNestedValue<T>
-): input is { [key: string]: ValueOrNestedValue<T> } => {
-  return (
-    !valueOrNestedValueIsString(input)
-    && !valueOrNestedValueIsStringArray(input)
-    && !valueOrNestedValueIsNestedValueArray(input)
-  );
-}
+): input is { [key: string]: ValueOrNestedValue<T> } => !valueOrNestedValueIsString(input) && !valueOrNestedValueIsStringArray(input);
